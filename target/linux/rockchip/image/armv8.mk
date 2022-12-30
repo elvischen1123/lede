155ae8b1932e44afb7cc2057b8152f99d6edc84b
coolsnowwolf <coolsnowwolf@gmail.com>
2022-12-30 12:18:52 +0800

    rockchip: add usb-serial-cp210x driver for H66K/H68K

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -18,7 +18,7 @@ define Device/hinlink_opc-h66k
   SOC := rk3568
   UBOOT_DEVICE_NAME := opc-h68k-rk3568
   IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-mt7921e kmod-r8125 wpad-openssl
+  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-mt7921e kmod-r8125 kmod-usb-serial-cp210x wpad-openssl
 endef
 TARGET_DEVICES += hinlink_opc-h66k
 
@@ -28,7 +28,7 @@ define Device/hinlink_opc-h68k
   SOC := rk3568
   UBOOT_DEVICE_NAME := opc-h68k-rk3568
   IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r5s | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-mt7921e kmod-r8125 wpad-openssl
+  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-mt7921e kmod-r8125 kmod-usb-serial-cp210x wpad-openssl
 endef
 TARGET_DEVICES += hinlink_opc-h68k
 
